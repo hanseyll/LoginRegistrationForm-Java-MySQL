9cd17a77625fbbb5f55e69e5eb312087d6d194f6
hansey <javierjose2555@gmail.com>
2023-01-08 21:17:21 -0500

    login working, now start dashboard

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -1,2 +1,94 @@
-public class LoginForm {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class LoginForm extends JDialog {
+    private JTextField tfEmail;
+    private JPasswordField pfPassword;
+    private JButton btnOK;
+    private JButton btnCancel;
+    private JPanel loginPanel;
+
+  public LoginForm(JFrame parent){
+      super(parent);
+      setTitle("Login");
+      setContentPane(loginPanel);
+      setMinimumSize(new Dimension(450,474));
+      setModal(true);
+      setLocationRelativeTo(parent);
+      setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+      btnOK.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+              String email = tfEmail.getText();
+              String password = String.valueOf(pfPassword.getPassword());
+               user = getAuthenticateUser(email,password);
+                if(user != null){
+                    dispose();
+                }else{
+                    JOptionPane.showMessageDialog(LoginForm.this,"email or password invalid","Try again",JOptionPane.ERROR_MESSAGE);
+                }
+          }
+      });
+      btnCancel.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+              dispose();
+          }
+      });
+
+      setVisible(true);
+  }
+
+  public User user;
+  private User getAuthenticateUser(String email, String password){
+      User user = null;
+
+      final String DB_URL = "jdbc:mysql://localhost/myStore?serverTimezone=UTC";
+      final String USERNAME = "root";
+      final String PASSWORD = "";
+
+      try{
+         Connection conn = DriverManager.getConnection(DB_URL,USERNAME,PASSWORD);
+         //connected to database successfully
+
+          Statement stmt = conn.createStatement();
+          String sql = "SELECT * FROM users WHERE email=? AND password=?";
+          PreparedStatement preparedStatement = conn.prepareStatement(sql);
+          preparedStatement.setString(1,email);
+          preparedStatement.setString(2,password);
+
+          ResultSet resultSet = preparedStatement.executeQuery();
+
+          if(resultSet.next()){
+              user = new User();
+              user.name = resultSet.getString("name");
+              user.email = resultSet.getString("email");
+              user.phone = resultSet.getString("phone");
+              user.address = resultSet.getString("address");
+              user.password = resultSet.getString("password");
+          }
+      }catch(Exception e){
+            e.printStackTrace();
+      }
+
+      return user;
+  }
+
+  public static void main(String[] args){
+      LoginForm loginForm = new LoginForm(null);
+      User user = loginForm.user;
+
+      if(user !=null){
+          System.out.println("Successful Auth of " + user.name);
+          System.out.println("Email: "+ user.email);
+          System.out.println("Phone: " + user.phone);
+          System.out.println("Address " + user.address);
+      }else{
+          System.out.println("auth cancelled");
+      }
+  }
 }
